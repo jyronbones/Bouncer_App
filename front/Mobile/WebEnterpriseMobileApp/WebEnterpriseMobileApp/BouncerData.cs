daae5ae33171f99841da8c05d7ea824bc4c9269d
Josh <JoshFearnall@gmail.com>
2023-11-30 22:47:49 -0500

    Added android application for project

---
+++ front/Mobile/WebEnterpriseMobileApp/WebEnterpriseMobileApp/BouncerData.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebEnterpriseMobileApp
+{
+    /// <summary>
+    /// Class used to model a bouncer object from the netbeans project
+    /// </summary>
+    public class BouncerData
+    {
+        /// <summary>
+        /// The bouncers Id
+        /// </summary>
+        [JsonProperty("id")]
+        public long Id { get; set; }
+        /// <summary>
+        /// The bouncers x position
+        /// </summary>
+        [JsonProperty("x")]
+        public int X { get; set; }
+        /// <summary>
+        /// The bouncers y position
+        /// </summary>
+        [JsonProperty("y")]
+        public int Y { get; set; }
+        /// <summary>
+        /// The bouncers y velocity
+        /// </summary>
+        [JsonProperty("YVelocity")]
+        public int YVelocity { get; set; }
+    }
+}
