17faf46b3c7cf5950456b088f5d12da1d0d3bbe3
Mohamed Elmursi <elmu0006@algonquinlive.com>
2023-12-08 14:59:37 -0500

    added implemnetation comments

--- back/bouncer-fearnall/src/test/java/tests/BouncerTest.java
+++ back/bouncer-fearnall/src/test/java/tests/BouncerTest.java
@@ -1,17 +1,26 @@
 package tests;
 import cst8218.joshua.bouncer.entity.Bouncer;
 import org.junit.jupiter.api.Test;
-
 import java.util.Random;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for the Bouncer class.
+ * @author : Mohamed Elmursi
+ * @version : 1.0
+ * Description : This class is used to test the Bouncer class.
+ */
 public class BouncerTest {
 
+    // Random number generator for creating random Bouncer objects
     private final Random random = new Random();
-        private final int FRAME_HEIGHT = 600;
+    // Frame height for testing wall collision
+    private final int FRAME_HEIGHT = 600;
 
-
+    /**
+     * Create a Bouncer object with random values for testing.
+     * @return Bouncer object with random values
+     */
     private Bouncer createRandomBouncer() {
         Bouncer bouncer = new Bouncer();
         bouncer.setId(random.nextLong());
@@ -21,8 +30,14 @@ public class BouncerTest {
         return bouncer;
     }
 
+    /**
+     * Test that a Bouncer object is created with valid values.
+     * @result Bouncer object should not be null and all values should be set
+     * to non-null values.
+     */
     @Test
     public void testCreateBouncerWithRandomValues() {
+        // Create a Bouncer object with random valuess
         Bouncer bouncer = createRandomBouncer();
         assertNotNull(bouncer, "Bouncer object should not be null");
         assertNotNull(bouncer.getId(), "ID should not be null");
@@ -30,13 +45,21 @@ public class BouncerTest {
         assertNotNull(bouncer.getY(), "Y position should not be null");
         assertNotNull(bouncer.getYVelocity(), "Y velocity should not be null");
     }
-    
+
+    /**
+     * Test AdvanceOneFrame method.
+     * @result Y velocity should increase by gravity, Y position should be updated
+     * based on velocity, and Y velocity should be reversed upon collision with wall.
+     */
     @Test
     public void testAdvanceOneFrame() {
+        // Create a Bouncer object with random values
         Bouncer bouncer = createRandomBouncer();
+        // Save initial values
         Integer initialY = bouncer.getY();
         Integer initialYVelocity = bouncer.getYVelocity();
 
+        // Advance one frame
         bouncer.advanceOneFrame();
 
         // Test that Y velocity is adjusted due to gravity
@@ -51,5 +74,5 @@ public class BouncerTest {
         }
     }
 
-    // Additional test methods here, each creating a new Bouncer instance
-}
+}// End of BouncerTest class
+
